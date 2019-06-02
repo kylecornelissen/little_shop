0525bf67d79894bccc048c091440c45e6758f50d
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-06-02 04:24:56 -0600

    Add home address creation to user create/registration in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      @address = Address.create(name: "home", address: user_params[:address], city: user_params[:city], state: user_params[:state], zip: user_params[:zip], user: @user)
       session[:user_id] = @user.id
       flash[:success] = "Registration Successful! You are now logged in."
       redirect_to profile_path
